74787cd05faf588702389ddb8816c8a53a5e9aa5
yasuonagaro <yasuo.nagaro@gmail.com>
2025-08-11 11:10:42 +0900

    Initial Commit

---
+++ ApiAppTests/ApiAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ApiAppTests.swift
+//  ApiAppTests
+//
+//  Created by ynagaro on 2025/08/11.
+//
+
+import Testing
+@testable import ApiApp
+
+struct ApiAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
