eec4edbb60e6630f5c3f2586e2c125a539920b45
yasuonagaro <yasuo.nagaro@gmail.com>
2025-08-11 12:39:37 +0900

    fix entrypoint

---
+++ ApiApp/ShopCell.swift
@@ -0,0 +1,23 @@
+//
+//  ShopCell.swift
+//  ApiApp
+//
+//  Created by ynagaro on 2025/08/11.
+//
+
+import UIKit
+
+class ShopCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
