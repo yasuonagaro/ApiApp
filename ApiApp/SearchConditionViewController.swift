cda9cc2b8b008361362c298cda0fb41cb0b4175a
yasuonagaro <yasuo.nagaro@gmail.com>
2025-08-24 23:11:58 +0900

    commit

---
+++ ApiApp/SearchConditionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchConditionViewController.swift
+//  ApiApp
+//
+//  Created by ynagaro on 2025/08/12.
+//
+
+import UIKit
+
+class SearchConditionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
